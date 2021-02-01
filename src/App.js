96df9a0500c996236d43428b2f33bd4706047bac
cagloria <33134245+cagloria@users.noreply.github.com>
2021-01-31 21:44:47 -0700

    Added base HTML

--- src/App.js
+++ src/App.js
@@ -1,25 +1,61 @@
-import logo from './logo.svg';
-import './App.css';
-
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <div className="App">
+            <p>Tuesday, July 14, 2020</p>
+            <p>11:55 AM</p>
+            <p>90&deg;</p>
+            <p>109&deg;&uarr; 83&deg;&darr;</p>
+            <p>Cloudy</p>
+            <div>
+                <p>Sunrise</p>
+                <p>6:00 AM</p>
+                <p>Sunset</p>
+                <p>6:00 PM</p>
+            </div>
+
+            <div>
+                <table>
+                    <tbody>
+                        <tr>
+                            <td>Wind</td>
+                            <td>10 mph</td>
+                        </tr>
+                        <tr>
+                            <td>Humidity</td>
+                            <td>18%</td>
+                        </tr>
+                        <tr>
+                            <td>Pressure</td>
+                            <td>1010 mb</td>
+                        </tr>
+                        <tr>
+                            <td>Visibility</td>
+                            <td>14.8 miles</td>
+                        </tr>
+                    </tbody>
+                </table>
+            </div>
+
+            <div>
+                <div>
+                    <p>Tomorrow</p>
+                    <p>109&deg;&uarr; 83&deg;&darr;</p>
+                </div>
+                <div>
+                    <p>Wednesday</p>
+                    <p>109&deg;&uarr; 83&deg;&darr;</p>
+                </div>
+                <div>
+                    <p>Thursday</p>
+                    <p>109&deg;&uarr; 83&deg;&darr;</p>
+                </div>
+            </div>
+
+            <div>
+                <p>Data provided by OpenWeather.</p>
+            </div>
+        </div>
+    );
 }
 
 export default App;
