92471950110c6bb1960c2a256d55130cd9112fc0
cagloria <33134245+cagloria@users.noreply.github.com>
2021-02-01 19:34:34 -0700

    Updated TodayWeather component
    - Now uses useEffect hook to fetch data
    - Updated return to show data

--- src/components/TodayWeather.js
+++ src/components/TodayWeather.js
@@ -1,36 +1,64 @@
+import React, { useState, useEffect } from "react";
 import Clock from "./Clock";
 
-export default function TodayWeather() {
-    const API_KEY = process.env.REACT_APP_WEATHER_API_KEY;
+const API = (() => {
+    const _key = process.env.REACT_APP_WEATHER_API_KEY;
     const url =
-        "http://api.openweathermap.org/data/2.5/weather?id=524901&appid=" +
-        API_KEY;
+        "http://api.openweathermap.org/data/2.5/weather?q=Phoenix&units=imperial&appid=" +
+        _key;
 
-    async function getWeather() {
-        try {
-            const response = await fetch(url);
-            const data = await response.json();
-            console.log(data);
-        } catch (error) {
-            console.log(error);
+    return { url };
+})();
+
+export default function TodayWeather() {
+    const [weatherData, setWeather] = useState(undefined);
+
+    useEffect(() => {
+        async function getWeather() {
+            try {
+                const response = await fetch(API.url);
+                const data = await response.json();
+                console.log(data);
+                setWeather(data);
+            } catch (error) {
+                console.log(error);
+            }
         }
+
+        getWeather();
+    }, []);
+
+    function convertFromUnix(time) {
+        const newTime = new Date(time * 1000);
+        return newTime.toLocaleTimeString();
     }
 
-    getWeather();
+    const data =
+        weatherData === undefined ? (
+            <p>Getting data...</p>
+        ) : (
+            <>
+                <p>{weatherData.name}</p>
+                <p>{weatherData.main.temp}&deg;</p>
+                <p>
+                    {weatherData.main.temp_max}&deg;&uarr;{" "}
+                    {weatherData.main.temp_min}
+                    &deg;&darr;
+                </p>
+                <p>{weatherData.weather[0].main}</p>
+                <div>
+                    <p>Sunrise</p>
+                    <p>{convertFromUnix(weatherData.sys.sunrise)}</p>
+                    <p>Sunset</p>
+                    <p>{convertFromUnix(weatherData.sys.sunset)}</p>
+                </div>
+            </>
+        );
 
     return (
         <div>
             <Clock />
-            <p>City</p>
-            <p>90&deg;</p>
-            <p>109&deg;&uarr; 83&deg;&darr;</p>
-            <p>Cloudy</p>
-            <div>
-                <p>Sunrise</p>
-                <p>6:00 AM</p>
-                <p>Sunset</p>
-                <p>6:00 PM</p>
-            </div>
+            {data}
         </div>
     );
 }
