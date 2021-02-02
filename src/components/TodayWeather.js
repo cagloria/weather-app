9c25f9213c6a2c2d0f867294c03c16c98b82b064
cagloria <33134245+cagloria@users.noreply.github.com>
2021-02-01 18:14:06 -0700

    Added API request

--- src/components/TodayWeather.js
+++ src/components/TodayWeather.js
@@ -1,9 +1,27 @@
 import Clock from "./Clock";
 
 export default function TodayWeather() {
+    const API_KEY = process.env.REACT_APP_WEATHER_API_KEY;
+    const url =
+        "http://api.openweathermap.org/data/2.5/weather?id=524901&appid=" +
+        API_KEY;
+
+    async function getWeather() {
+        try {
+            const response = await fetch(url);
+            const data = await response.json();
+            console.log(data);
+        } catch (error) {
+            console.log(error);
+        }
+    }
+
+    getWeather();
+
     return (
         <div>
             <Clock />
+            <p>City</p>
             <p>90&deg;</p>
             <p>109&deg;&uarr; 83&deg;&darr;</p>
             <p>Cloudy</p>
