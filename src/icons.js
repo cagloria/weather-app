ad9d5396d413b309421507c84dad1edc76ce81c2
cagloria <33134245+cagloria@users.noreply.github.com>
2021-03-15 16:41:00 -0700

    Added atmosphere icons

--- src/icons.js
+++ src/icons.js
@@ -4,6 +4,12 @@ import iconRainLight from "./assets/icon-rain-light.svg";
 import iconRainHeavy from "./assets/icon-rain-heavy.svg";
 import iconSleet from "./assets/icon-snow-sleet.svg";
 import iconSnow from "./assets/icon-snow.svg";
+import iconMist from "./assets/icon-mist-haze-fog.svg";
+import iconSmoke from "./assets/icon-smoke.svg";
+import iconDust from "./assets/icon-dust-sand.svg";
+import iconAsh from "./assets/icon-ash.svg";
+import iconWind from "./assets/icon-wind.svg";
+import iconTornado from "./assets/icon-tornado.svg";
 import iconClear from "./assets/icon-clear.svg";
 import iconCloudsLight from "./assets/icon-cloud-light.svg";
 import iconCloudsHeavy from "./assets/icon-cloud-heavy.svg";
@@ -17,6 +23,12 @@ export const icons = {
     rainHeavy: iconRainHeavy,
     sleet: iconSleet,
     snow: iconSnow,
+    smoke: iconSmoke,
+    mist: iconMist,
+    dust: iconDust,
+    ash: iconAsh,
+    wind: iconWind,
+    tornado: iconTornado,
     clear: iconClear,
     cloudsLight: iconCloudsLight,
     cloudsHeavy: iconCloudsHeavy,
@@ -42,6 +54,19 @@ export function findWeatherIcon(code) {
             return icons.snow;
         } else if (code <= 799) {
             // Atmosphere
+            if (code === 701 || code === 721 || code === 741) {
+                return icons.mist;
+            } else if (code === 711) {
+                return icons.smoke;
+            } else if (code === 731 || code === 751 || code === 761) {
+                return icons.dust;
+            } else if (code === 762) {
+                return icons.ash;
+            } else if (code === 771) {
+                return icons.wind;
+            } else if (code === 781) {
+                return icons.tornado;
+            }
         } else if (code === 800) {
             return icons.clear;
         } else if (code === 801) {
